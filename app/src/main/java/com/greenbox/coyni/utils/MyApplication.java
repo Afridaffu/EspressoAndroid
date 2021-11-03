d6dbca392707727f976083c765611f80c04fb2ce
ideyaLabs <8074192078>
2021-11-03 11:55:21 +0530

    Account Limits Screen

--- app/src/main/java/com/greenbox/coyni/utils/MyApplication.java
+++ app/src/main/java/com/greenbox/coyni/utils/MyApplication.java
@@ -2,5 +2,250 @@ package com.greenbox.coyni.utils;
 
 import android.app.Application;
 
+import com.greenbox.coyni.model.cards.CardsDataItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyApplication extends Application {
+    static String strEncryptedPublicKey;
+    List<CardsDataItem> listCards = new ArrayList<>();
+
+
+    //Account Limits
+    private double tokenWithdrawalBankDayLimit;
+    private double tokenWithdrawalBankWeekLimit;
+
+    private double tokenWithdrawalInstantpayDayLimit;
+    private double tokenWithdrawalInstantpayWeekLimit;
+
+    private double tokenWithdrawalGiftcardDayLimit;
+    private double tokenWithdrawalGiftcardWeekLimit;
+
+    private double tokenSendDayLimit;
+    private double tokenSendWeekLimit;
+
+    private double tokenBuyBankDayLimit;
+    private double tokenBuyBankWeeekLimit;
+
+    private double tokenBuyCardDayLimit;
+    private double tokenBuyCardWeekLimit;
+
+    private double tokenWithdrawalSignetDayLimit;
+    private double tokenWithdrawalSignetWeekLimit;
+
+    int intUserId;
+    String strCity, strPhoneNum, strState, strCountry, strAddressLine1, strAddressLine2, strZipCode, strProfileImg;
+
+    public int getIntUserId() {
+        return intUserId;
+    }
+
+    public void setIntUserId(int intUserId) {
+        this.intUserId = intUserId;
+    }
+
+    public String getStrCity() {
+        return strCity;
+    }
+
+    public void setStrCity(String strCity) {
+        this.strCity = strCity;
+    }
+
+    public String getStrPhoneNum() {
+        return strPhoneNum;
+    }
+
+    public void setStrPhoneNum(String strPhoneNum) {
+        this.strPhoneNum = strPhoneNum;
+    }
+
+    public String getStrState() {
+        return strState;
+    }
+
+    public void setStrState(String strState) {
+        this.strState = strState;
+    }
+
+    public String getStrCountry() {
+        return strCountry;
+    }
+
+    public void setStrCountry(String strCountry) {
+        this.strCountry = strCountry;
+    }
+
+    public String getStrAddressLine1() {
+        return strAddressLine1;
+    }
+
+    public void setStrAddressLine1(String strAddressLine1) {
+        this.strAddressLine1 = strAddressLine1;
+    }
+
+    public String getStrAddressLine2() {
+        return strAddressLine2;
+    }
+
+    public void setStrAddressLine2(String strAddressLine2) {
+        this.strAddressLine2 = strAddressLine2;
+    }
+
+    public String getStrZipCode() {
+        return strZipCode;
+    }
+
+    public void setStrZipCode(String strZipCode) {
+        this.strZipCode = strZipCode;
+    }
+
+    public String getStrProfileImg() {
+        return strProfileImg;
+    }
+
+    public void setStrProfileImg(String strProfileImg) {
+        this.strProfileImg = strProfileImg;
+    }
+
+    public double getTokenWithdrawalBankDayLimit() {
+        return tokenWithdrawalBankDayLimit;
+    }
+
+    public void setTokenWithdrawalBankDayLimit(double tokenWithdrawalBankDayLimit) {
+        this.tokenWithdrawalBankDayLimit = tokenWithdrawalBankDayLimit;
+    }
+
+    public double getTokenWithdrawalBankWeekLimit() {
+        return tokenWithdrawalBankWeekLimit;
+    }
+
+    public void setTokenWithdrawalBankWeekLimit(double tokenWithdrawalBankWeekLimit) {
+        this.tokenWithdrawalBankWeekLimit = tokenWithdrawalBankWeekLimit;
+    }
+
+    public double getTokenWithdrawalInstantpayDayLimit() {
+        return tokenWithdrawalInstantpayDayLimit;
+    }
+
+    public void setTokenWithdrawalInstantpayDayLimit(double tokenWithdrawalInstantpayDayLimit) {
+        this.tokenWithdrawalInstantpayDayLimit = tokenWithdrawalInstantpayDayLimit;
+    }
+
+    public double getTokenWithdrawalInstantpayWeekLimit() {
+        return tokenWithdrawalInstantpayWeekLimit;
+    }
+
+    public void setTokenWithdrawalInstantpayWeekLimit(double tokenWithdrawalInstantpayWeekLimit) {
+        this.tokenWithdrawalInstantpayWeekLimit = tokenWithdrawalInstantpayWeekLimit;
+    }
+
+    public double getTokenWithdrawalGiftcardDayLimit() {
+        return tokenWithdrawalGiftcardDayLimit;
+    }
+
+    public void setTokenWithdrawalGiftcardDayLimit(double tokenWithdrawalGiftcardDayLimit) {
+        this.tokenWithdrawalGiftcardDayLimit = tokenWithdrawalGiftcardDayLimit;
+    }
+
+    public double getTokenWithdrawalGiftcardWeekLimit() {
+        return tokenWithdrawalGiftcardWeekLimit;
+    }
+
+    public void setTokenWithdrawalGiftcardWeekLimit(double tokenWithdrawalGiftcardWeekLimit) {
+        this.tokenWithdrawalGiftcardWeekLimit = tokenWithdrawalGiftcardWeekLimit;
+    }
+
+    public double getTokenSendDayLimit() {
+        return tokenSendDayLimit;
+    }
+
+    public void setTokenSendDayLimit(double tokenSendDayLimit) {
+        this.tokenSendDayLimit = tokenSendDayLimit;
+    }
+
+    public double getTokenSendWeekLimit() {
+        return tokenSendWeekLimit;
+    }
+
+    public void setTokenSendWeekLimit(double tokenSendWeekLimit) {
+        this.tokenSendWeekLimit = tokenSendWeekLimit;
+    }
+
+    public double getTokenBuyBankDayLimit() {
+        return tokenBuyBankDayLimit;
+    }
+
+    public void setTokenBuyBankDayLimit(double tokenBuyBankDayLimit) {
+        this.tokenBuyBankDayLimit = tokenBuyBankDayLimit;
+    }
+
+    public double getTokenBuyBankWeeekLimit() {
+        return tokenBuyBankWeeekLimit;
+    }
+
+    public void setTokenBuyBankWeeekLimit(double tokenBuyBankWeeekLimit) {
+        this.tokenBuyBankWeeekLimit = tokenBuyBankWeeekLimit;
+    }
+
+    public double getTokenBuyCardDayLimit() {
+        return tokenBuyCardDayLimit;
+    }
+
+    public void setTokenBuyCardDayLimit(double tokenBuyCardDayLimit) {
+        this.tokenBuyCardDayLimit = tokenBuyCardDayLimit;
+    }
+
+    public double getTokenBuyCardWeekLimit() {
+        return tokenBuyCardWeekLimit;
+    }
+
+    public void setTokenBuyCardWeekLimit(double tokenBuyCardWeekLimit) {
+        this.tokenBuyCardWeekLimit = tokenBuyCardWeekLimit;
+    }
+
+    public double getTokenWithdrawalSignetDayLimit() {
+        return tokenWithdrawalSignetDayLimit;
+    }
+
+    public void setTokenWithdrawalSignetDayLimit(double tokenWithdrawalSignetDayLimit) {
+        this.tokenWithdrawalSignetDayLimit = tokenWithdrawalSignetDayLimit;
+    }
+
+    public double getTokenWithdrawalSignetWeekLimit() {
+        return tokenWithdrawalSignetWeekLimit;
+    }
+
+    public void setTokenWithdrawalSignetWeekLimit(double tokenWithdrawalSignetWeekLimit) {
+        this.tokenWithdrawalSignetWeekLimit = tokenWithdrawalSignetWeekLimit;
+    }
+
+    public static String getStrEncryptedPublicKey() {
+        return strEncryptedPublicKey;
+    }
+
+    public void setStrEncryptedPublicKey(String strEncryptedPublicKey) {
+        this.strEncryptedPublicKey = strEncryptedPublicKey;
+    }
+
+    public List<CardsDataItem> getListCards() {
+        return listCards;
+    }
+
+    public void setListCards(List<CardsDataItem> listCards) {
+        this.listCards = listCards;
+    }
+
+    private String fromWhichFragment;
+
+    public String getFromWhichFragment() {
+        return fromWhichFragment;
+    }
+
+    public void setFromWhichFragment(String fromWhichFragment) {
+        this.fromWhichFragment = fromWhichFragment;
+    }
+
+
 }
---
+++ app/src/main/java/com/greenbox/coyni/view/AccountLimitsActivity.java
@@ -0,0 +1,47 @@
+package com.greenbox.coyni.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.greenbox.coyni.R;
+import com.greenbox.coyni.model.users.AccountLimits;
+import com.greenbox.coyni.viewmodel.AccountLimitsViewModel;
+
+public class AccountLimitsActivity extends AppCompatActivity {
+
+    AccountLimitsViewModel accountLimitsViewModel;
+    ProgressDialog dialog;
+    int userType = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_limits);
+
+        accountLimitsViewModel = new ViewModelProvider(this).get(AccountLimitsViewModel.class);
+        initObserver();
+        accountLimitsViewModel.meAccountLimits(userType);
+
+
+    }
+
+    public void initObserver(){
+
+        accountLimitsViewModel.getUserAccountLimitsMutableLiveData().observe(this, new Observer<AccountLimits>() {
+            @Override
+            public void onChanged(AccountLimits accountLimits) {
+
+
+
+
+
+            }
+        });
+    }
+}
