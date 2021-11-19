a04ebe170299591a6dc574c33ecaa183650314ff
sandhyab1234 <SandhyaB@ideyalabs.com>
2021-11-19 16:20:18 +0530

    UI Changes S2

---
+++ app/src/main/java/com/greenbox/coyni/view/WithdrawGiftCard.java
@@ -0,0 +1,16 @@
+package com.greenbox.coyni.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.greenbox.coyni.R;
+
+public class WithdrawGiftCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_withdraw_gift_card);
+    }
+}
