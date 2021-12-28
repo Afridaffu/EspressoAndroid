61f704882c31cc3852bbda0f6731ef6be62c9556
Kiran Chalasani <kiranc@ideyalabs.com>
2021-12-28 15:00:18 +0530

    WithdrawToken InstantPay Implementation

--- app/src/main/java/com/greenbox/coyni/network/ApiService.java
+++ app/src/main/java/com/greenbox/coyni/network/ApiService.java
@@ -84,6 +84,8 @@ import com.greenbox.coyni.model.users.UserData;
 import com.greenbox.coyni.model.users.UserPreferenceModel;
 import com.greenbox.coyni.model.wallet.UserDetails;
 import com.greenbox.coyni.model.wallet.WalletResponse;
+import com.greenbox.coyni.model.withdraw.WithdrawRequest;
+import com.greenbox.coyni.model.withdraw.WithdrawResponse;
 
 import java.util.Map;
 
@@ -293,4 +295,7 @@ public interface ApiService {
 
     @GET("api/v2/giftcard/giftCardBrands")
     Call<BrandsResponse> getGiftCards();
+
+    @POST("api/v2/node/withdrawTokens")
+    Call<WithdrawResponse> withdrawTokens(@Body WithdrawRequest request);
 }
--- app/src/main/java/com/greenbox/coyni/viewmodel/BuyTokenViewModel.java
+++ app/src/main/java/com/greenbox/coyni/viewmodel/BuyTokenViewModel.java
@@ -17,6 +17,8 @@ import com.greenbox.coyni.model.transactionlimit.TransactionLimitRequest;
 import com.greenbox.coyni.model.transactionlimit.TransactionLimitResponse;
 import com.greenbox.coyni.model.transferfee.TransferFeeRequest;
 import com.greenbox.coyni.model.transferfee.TransferFeeResponse;
+import com.greenbox.coyni.model.withdraw.WithdrawRequest;
+import com.greenbox.coyni.model.withdraw.WithdrawResponse;
 import com.greenbox.coyni.network.ApiService;
 import com.greenbox.coyni.network.AuthApiClient;
 
@@ -32,6 +34,8 @@ public class BuyTokenViewModel extends AndroidViewModel {
     private MutableLiveData<TransferFeeResponse> transferFeeResponseMutableLiveData = new MutableLiveData<>();
     private MutableLiveData<BuyTokenResponse> buyTokResponseMutableLiveData = new MutableLiveData<>();
     private MutableLiveData<BuyTokenResponse> buyTokenFailureMutableLiveData = new MutableLiveData<>();
+    private MutableLiveData<WithdrawResponse> withdrawResponseMutableLiveData = new MutableLiveData<>();
+    private MutableLiveData<WithdrawResponse> withdrawFailureResponseMutableLiveData = new MutableLiveData<>();
 
     public BuyTokenViewModel(@NonNull Application application) {
         super(application);
@@ -55,6 +59,14 @@ public class BuyTokenViewModel extends AndroidViewModel {
 
     public MutableLiveData<BuyTokenResponse> getBuyTokenFailureMutableLiveData() {
         return buyTokenFailureMutableLiveData;
+    }
+
+    public MutableLiveData<WithdrawResponse> getWithdrawResponseMutableLiveData() {
+        return withdrawResponseMutableLiveData;
+    }
+
+    public MutableLiveData<WithdrawResponse> getWithdrawFailureResponseMutableLiveData() {
+        return withdrawFailureResponseMutableLiveData;
     }
 
     public void transactionLimits(TransactionLimitRequest request, int userType) {
@@ -174,6 +186,54 @@ public class BuyTokenViewModel extends AndroidViewModel {
                 public void onFailure(Call<BuyTokenResponse> call, Throwable t) {
                     Toast.makeText(getApplication(), "something went wrong", Toast.LENGTH_LONG).show();
                     apiErrorMutableLiveData.setValue(null);
+                }
+            });
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void withdrawTokens(WithdrawRequest request) {
+        try {
+            ApiService apiService = AuthApiClient.getInstance().create(ApiService.class);
+            Call<WithdrawResponse> mCall = apiService.withdrawTokens(request);
+            mCall.enqueue(new Callback<WithdrawResponse>() {
+                @Override
+                public void onResponse(Call<WithdrawResponse> call, Response<WithdrawResponse> response) {
+                    try {
+                        String strResponse = "";
+                        if (response.isSuccessful()) {
+                            WithdrawResponse obj = response.body();
+                            withdrawResponseMutableLiveData.setValue(obj);
+                        } else if (response.code() == 400) {
+                            strResponse = response.errorBody().string().replaceAll("\"", "'");
+                            Gson gson = new Gson();
+                            Type type = new TypeToken<WithdrawResponse>() {
+                            }.getType();
+                            WithdrawResponse errorResponse = gson.fromJson(response.errorBody().string(), type);
+                            if (errorResponse != null) {
+                                withdrawFailureResponseMutableLiveData.setValue(errorResponse);
+                            } else {
+                                WithdrawResponse errorResponse1 = gson.fromJson(strResponse, type);
+                                withdrawFailureResponseMutableLiveData.setValue(errorResponse1);
+                            }
+                        } else {
+                            Gson gson = new Gson();
+                            Type type = new TypeToken<APIError>() {
+                            }.getType();
+                            APIError errorResponse = gson.fromJson(response.errorBody().string(), type);
+                            apiErrorMutableLiveData.setValue(errorResponse);
+                        }
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                        withdrawResponseMutableLiveData.setValue(null);
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<WithdrawResponse> call, Throwable t) {
+                    Toast.makeText(getApplication(), "something went wrong", Toast.LENGTH_LONG).show();
+                    withdrawResponseMutableLiveData.setValue(null);
                 }
             });
         } catch (Exception ex) {
