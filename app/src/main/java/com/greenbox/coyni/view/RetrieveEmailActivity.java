46f0d2822f93676986d1868007d54e18e1f7b8c4
ideyaLabs <8074192078>
2022-01-17 18:57:24 +0530

    Bug fixes

--- app/src/main/java/com/greenbox/coyni/view/RetrieveEmailActivity.java
+++ app/src/main/java/com/greenbox/coyni/view/RetrieveEmailActivity.java
@@ -97,7 +97,6 @@ public class RetrieveEmailActivity extends AppCompatActivity implements TextWatc
             else if (firstName.getText().toString().trim().length() == 0) {
                 firstNameErrorLL.setVisibility(VISIBLE);
                 firstNameErrorTV.setText("Field Required");
-                firstTIL.setBoxStrokeColorStateList(Utils.getErrorColorState());
             }
             enableButton();
         } else if (charSequence == lastName.getEditableText()) {
@@ -109,7 +108,6 @@ public class RetrieveEmailActivity extends AppCompatActivity implements TextWatc
             else if (lastName.getText().toString().trim().length() == 0) {
                 lastNameErrorLL.setVisibility(VISIBLE);
                 lastNameErrorTV.setText("Field Required");
-                lastTIL.setBoxStrokeColorStateList(Utils.getErrorColorState());
             }
             enableButton();
         }
