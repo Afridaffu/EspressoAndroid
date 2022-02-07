ee8421894cfe2a2acb63fa84107233aafb883de5
sandhyab1234 <SandhyaB@ideyalabs.com>
2022-02-07 14:26:16 +0530

    Open new account screens

--- app/src/main/java/com/greenbox/coyni/view/business/BusinessDashboardMainActivity.java
+++ app/src/main/java/com/greenbox/coyni/view/business/BusinessDashboardMainActivity.java
@@ -7,10 +7,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.greenbox.coyni.R;
 
 public class BusinessDashboardMainActivity extends AppCompatActivity {
+//    ImageView merchantprofile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_business_dashboard_main);
+
+//        merchantprofile = findViewById(R.id.merchantprofileIV);
+//        merchantprofile.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(BusinessDashboardMainActivity.this, MerchantProfile.class);
+//                startActivity(intent);
+//            }
+//        });
     }
 }
