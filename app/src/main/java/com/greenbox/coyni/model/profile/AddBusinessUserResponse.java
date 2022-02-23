88160a1766f6e5e4c247d3fb6d954212043f8aa6
SahooNishant <97294821+SahooNishant@users.noreply.github.com>
2022-02-23 10:41:42 +0530

    added AddBusinessUserResponse class file

---
+++ app/src/main/java/com/greenbox/coyni/model/profile/AddBusinessUserResponse.java
@@ -0,0 +1,111 @@
+package com.greenbox.coyni.model.profile;
+
+import com.greenbox.coyni.model.Error;
+
+public class AddBusinessUserResponse {
+
+    private String status;
+    private String timestamp;
+    private TrackerData data;
+    private Error error;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public TrackerData getData() {
+        return data;
+    }
+
+    public void setData(TrackerData data) {
+        this.data = data;
+    }
+
+    public Error getError() {
+        return error;
+    }
+
+    public void setError(Error error) {
+        this.error = error;
+    }
+
+    public class TrackerData{
+        private boolean isProfileVerified;
+        private boolean isAuthyRegistered;
+        private boolean isAddressAvailable;
+        private boolean isPaymentModeAdded;
+        private boolean isPersonIdentified;
+        private boolean isAgreementSigned;
+
+        private String jwtToken;
+
+        public String getJwtToken() {
+            return jwtToken;
+        }
+
+        public void setJwtToken(String jwtToken) {
+            this.jwtToken = jwtToken;
+        }
+
+
+        public boolean isProfileVerified() {
+            return isProfileVerified;
+        }
+
+        public void setProfileVerified(boolean profileVerified) {
+            isProfileVerified = profileVerified;
+        }
+
+        public boolean isAuthyRegistered() {
+            return isAuthyRegistered;
+        }
+
+        public void setAuthyRegistered(boolean authyRegistered) {
+            isAuthyRegistered = authyRegistered;
+        }
+
+        public boolean isAddressAvailable() {
+            return isAddressAvailable;
+        }
+
+        public void setAddressAvailable(boolean addressAvailable) {
+            isAddressAvailable = addressAvailable;
+        }
+
+        public boolean isPaymentModeAdded() {
+            return isPaymentModeAdded;
+        }
+
+        public void setPaymentModeAdded(boolean paymentModeAdded) {
+            isPaymentModeAdded = paymentModeAdded;
+        }
+
+        public boolean isPersonIdentified() {
+            return isPersonIdentified;
+        }
+
+        public void setPersonIdentified(boolean personIdentified) {
+            isPersonIdentified = personIdentified;
+        }
+
+        public boolean isAgreementSigned() {
+            return isAgreementSigned;
+        }
+
+        public void setAgreementSigned(boolean agreementSigned) {
+            isAgreementSigned = agreementSigned;
+        }
+    }
+}
