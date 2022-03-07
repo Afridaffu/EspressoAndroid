72bc5b76bdd2b36e70aaaaae3c2e004e26783295
SahooNishant <97294821+SahooNishant@users.noreply.github.com>
2022-03-07 15:20:12 +0530

    webview changes

--- app/src/main/java/com/greenbox/coyni/view/business/WebViewShowFileActivity.java
+++ app/src/main/java/com/greenbox/coyni/view/business/WebViewShowFileActivity.java
@@ -1,6 +1,7 @@
 package com.greenbox.coyni.view.business;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
@@ -13,12 +14,15 @@ import android.webkit.WebViewClient;
 import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.bumptech.glide.Glide;
+import com.google.android.material.progressindicator.CircularProgressIndicator;
 import com.greenbox.coyni.R;
 import com.greenbox.coyni.adapters.BusinessProfileRecyclerAdapter;
 import com.greenbox.coyni.model.preferences.ProfilesResponse;
@@ -38,13 +42,15 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-public class WebViewShowFileActivity extends BaseActivity {
+public class WebViewShowFileActivity extends AppCompatActivity {
 
     private WebView webView;
     private ImageView imageView;
     private static final String HTML_FORMAT = "<html><body style=\"text-align: center; background-color: black; vertical-align: center;\"><img width=\"300\" src = \"%s\" /></body></html>";
     private LinearLayout llClose;
     private ImageView btnClose;
+    private RelativeLayout layoutLoader;
+    private CircularProgressIndicator cpProgress;
     private String fileURL;
 
 
@@ -66,19 +72,50 @@ public class WebViewShowFileActivity extends BaseActivity {
             btnClose =(ImageView)findViewById(R.id.closeBtn);
 
            // webView.setBackgroundColor(getResources().getColor(R.color.background_color));
+            layoutLoader = findViewById(R.id.layoutLoader);
+            cpProgress = findViewById(R.id.cpProgress);
+
+            layoutLoader.setVisibility(View.VISIBLE);
+            cpProgress.show();
+
 
             String extension = fileURL.substring(fileURL.lastIndexOf(".") + 1);
 
             LogUtils.d("extension","extension"+extension);
             LogUtils.d("extension","fillee"+fileURL.replaceAll(" ","%20"));
 
-            if(extension.equalsIgnoreCase("pdf")){
-                webView.loadUrl("https://docs.google.com/gview?embedded=true&url=" + fileURL.replaceAll(" ","%20"));
-            } else {
-                final String html = String.format(HTML_FORMAT, fileURL);
-                webView.loadDataWithBaseURL("", html, "text/html", "UTF-8", "");
+            webView.setWebViewClient(new WebViewClient() {
+                @Override
+                public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                    super.onPageStarted(view, url, favicon);
+                    try {
+                        if(extension.equalsIgnoreCase("pdf")){
+                            webView.loadUrl("https://docs.google.com/gview?embedded=true&url=" + fileURL.replaceAll(" ","%20"));
+                            layoutLoader.setVisibility(View.GONE);
+                        } else {
+                            final String html = String.format(HTML_FORMAT, fileURL);
+                            webView.loadDataWithBaseURL("", html, "text/html", "UTF-8", "");
+                            layoutLoader.setVisibility(View.GONE);
+                        }
+                            onBackPressed();
+                            finish();
 
-            }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                public void onPageFinished(WebView view, String url) {
+                    try {
+
+                            findViewById(R.id.webView).setVisibility(View.VISIBLE);
+                            findViewById(R.id.layoutLoader).setVisibility(View.GONE);
+
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            });
 
             btnClose.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -87,12 +124,9 @@ public class WebViewShowFileActivity extends BaseActivity {
                 }
             });
 
-
         }   catch (Exception ex) {
             ex.printStackTrace();
         }
-
-
 
     }
 
