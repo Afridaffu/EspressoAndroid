dd20cf62d8de1f8488bdc67d727364d1baf7f31f
Ganesh V <ganeshv@ideyalabs.com>
2022-03-14 15:22:47 +0530

    4 activities converted into merchant transaction details

--- app/src/main/java/com/greenbox/coyni/view/business/RefundTransactionActivity.java
+++ app/src/main/java/com/greenbox/coyni/view/business/RefundTransactionActivity.java
@@ -49,6 +49,7 @@ public class RefundTransactionActivity extends AppCompatActivity {
     boolean isAuthenticationCalled = false, isPayClickable = false, isReqClickable = false, isPayClick = false;
     public static RefundTransactionActivity refundTransactionActivity;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
