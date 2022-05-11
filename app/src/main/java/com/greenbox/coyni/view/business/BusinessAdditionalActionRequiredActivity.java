f2383d0d068195b23cc08e19a7975afe1f822e8c
Sandhya B <sandhyaB@ideyalabs.com>
2022-05-11 12:02:24 +0530

    Bug fixes in Additional Action and team feature

--- app/src/main/java/com/greenbox/coyni/view/business/BusinessAdditionalActionRequiredActivity.java
+++ app/src/main/java/com/greenbox/coyni/view/business/BusinessAdditionalActionRequiredActivity.java
@@ -109,6 +109,7 @@ public class BusinessAdditionalActionRequiredActivity extends BaseActivity {
             super.onBackPressed();
         }
     }
+
     private void initFields() {
 
         additionReservedLL = findViewById(R.id.lladditionReserve);
@@ -448,7 +449,6 @@ public class BusinessAdditionalActionRequiredActivity extends BaseActivity {
                                     mLastClickTime = SystemClock.elapsedRealtime();
                                     View v = (View) view.getTag();
                                     showCommentDialog(v);
-
                                     Utils.shwForcedKeypad(BusinessAdditionalActionRequiredActivity.this);
                                 }
                             });
@@ -491,7 +491,7 @@ public class BusinessAdditionalActionRequiredActivity extends BaseActivity {
                     tvRemarks.setVisibility(View.VISIBLE);
                     llAccept.setVisibility(View.GONE);
                     tvDeclinedMsg.setVisibility(View.VISIBLE);
-                    tvDeclinedMsg.setText(getString(R.string.Decline) + " " + Utils.getCurrentDate() + " due to : ");
+                    tvDeclinedMsg.setText(getString(R.string.Decline) + " " + Utils.getCurrentDate() + " due to: ");
                     llDecline.setVisibility(View.GONE);
                     proposalsMap.get(tv.getText().toString()).setUserAccepted(false);
                     proposalsMap.get(tv.getText().toString()).setUserMessage(comm);
@@ -508,7 +508,7 @@ public class BusinessAdditionalActionRequiredActivity extends BaseActivity {
         dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
             @Override
             public void onDismiss(DialogInterface dialogInterface) {
-                Utils.hideKeypad(BusinessAdditionalActionRequiredActivity.this);
+                Utils.hideSoftKeyboard(BusinessAdditionalActionRequiredActivity.this);
             }
         });
     }
@@ -790,15 +790,14 @@ public class BusinessAdditionalActionRequiredActivity extends BaseActivity {
             LogUtils.d(TAG, "fileUpload" + fileUpload);
             if (fileUpload.containsValue(null)) {
                 isSubmitEnabled = false;
-                submitCV.setCardBackgroundColor(getResources().getColor(R.color.inactive_color));
                 submitCV.setClickable(false);
                 submitCV.setEnabled(false);
-
+                submitCV.setCardBackgroundColor(getResources().getColor(R.color.inactive_color));
             } else {
                 isSubmitEnabled = true;
-                submitCV.setCardBackgroundColor(getResources().getColor(R.color.primary_color));
                 submitCV.setClickable(true);
                 submitCV.setEnabled(true);
+                submitCV.setCardBackgroundColor(getResources().getColor(R.color.primary_color));
             }
 
         } catch (Exception e) {
