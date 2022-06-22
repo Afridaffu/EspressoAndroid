6c3fefa97df4d4753ab7fdb4617017cdf92f3557
ShivaS <shivas@ideyalabs.com>
2022-06-22 16:46:50 +0530

    Bug Fixes, Alignment Changes,Check Out Changes

--- app/src/main/java/com/greenbox/coyni/utils/MyApplication.java
+++ app/src/main/java/com/greenbox/coyni/utils/MyApplication.java
@@ -894,4 +894,23 @@ public class MyApplication extends Application {
         }
         return isFaceLock;
     }
+
+    public  PaymentMethodsResponse filterCheckPaymentMethods(PaymentMethodsResponse objResponse) {
+        PaymentMethodsResponse payMethodsResponse = objResponse;
+        List<PaymentsList> listData = new ArrayList<>();
+        try {
+            if (objResponse != null && objResponse.getData() != null && objResponse.getData().getData() != null && objResponse.getData().getData().size() > 0) {
+                for (int i = 0; i < objResponse.getData().getData().size(); i++) {
+                    if (!objResponse.getData().getData().get(i).getPaymentMethod().equalsIgnoreCase("bank")) {
+                        listData.add(objResponse.getData().getData().get(i));
+                    }
+                }
+                payMethodsResponse.getData().setData(listData);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return payMethodsResponse;
+    }
+
 }
