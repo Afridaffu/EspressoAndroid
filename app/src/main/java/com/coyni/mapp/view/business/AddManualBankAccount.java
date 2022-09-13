b14ce1a91f247414e7d6045e38664b1c7687728f
sandhyab1234 <sandhyab@ideyalabs.com>
2022-09-13 15:16:43 +0530

    Add manual bank account new screen created

---
+++ app/src/main/java/com/coyni/mapp/view/business/AddManualBankAccount.java
@@ -0,0 +1,16 @@
+package com.coyni.mapp.view.business;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.greenbox.coyni.R;
+
+public class AddManualBankAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_manual_bank_account);
+    }
+}
