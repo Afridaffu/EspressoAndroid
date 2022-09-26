01c8d8f6de0a277c2c31922f17ea4a984e5f2bec
ShivaS <shivas@ideyalabs.com>
2022-09-26 16:54:15 +0530

    Scan QR changes

--- app/src/main/java/com/coyni/mapp/view/business/PayToMerchantActivity.java
+++ app/src/main/java/com/coyni/mapp/view/business/PayToMerchantActivity.java
@@ -845,8 +845,11 @@ public class PayToMerchantActivity extends AppCompatActivity implements TextWatc
             public void onClick(View view) {
                 dialog.dismiss();
                 objMyApplication.setStrScreen("payRequest");
-                Intent i = new Intent(PayToMerchantActivity.this, BuyTokenPaymentMethodsActivity.class);
+                Intent i = new Intent(PayToMerchantActivity.this, SelectPaymentMethodActivity.class);
                 i.putExtra("screen", "payRequest");
+                if (objMyApplication.getAccountType() == Utils.BUSINESS_ACCOUNT) {
+                    i.putExtra("menuitem","buy");
+                }
                 startActivity(i);
                 //finish();
             }
