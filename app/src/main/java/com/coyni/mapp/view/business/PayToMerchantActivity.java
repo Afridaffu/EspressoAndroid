29178eb1c7f687d2fd934f3a3324c5748cbb608a
ShivaS <shivas@ideyalabs.com>
2022-09-28 18:46:31 +0530

    bug fix

--- app/src/main/java/com/coyni/mapp/view/business/PayToMerchantActivity.java
+++ app/src/main/java/com/coyni/mapp/view/business/PayToMerchantActivity.java
@@ -845,9 +845,16 @@ public class PayToMerchantActivity extends AppCompatActivity implements TextWatc
             public void onClick(View view) {
                 dialog.dismiss();
                 objMyApplication.setStrScreen("payRequest");
-                Intent i = new Intent(PayToMerchantActivity.this, SelectPaymentMethodActivity.class);
-                i.putExtra("screen", "payRequest");
-                i.putExtra("menuitem","buy");
+                Intent i;
+                if (objMyApplication.getAccountType() == Utils.PERSONAL_ACCOUNT) {
+                    i = new Intent(PayToMerchantActivity.this, BuyTokenPaymentMethodsActivity.class);
+                    i.putExtra("screen", "payRequest");
+                }
+                else {
+                    i = new Intent(PayToMerchantActivity.this, SelectPaymentMethodActivity.class);
+                    i.putExtra("screen", "payRequest");
+                    i.putExtra("menuitem","buy");
+                }
                 startActivity(i);
                 //finish();
             }
