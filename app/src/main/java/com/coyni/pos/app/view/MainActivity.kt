a54762ee0be1c6463d881a3a71404ebf0c98faa3
KrishnaV <krishnav@ideyalabs.com>
2023-02-14 19:02:06 +0530

    Initial commit

--- app/src/main/java/com/coyni/pos/app/view/MainActivity.kt
+++ app/src/main/java/com/coyni/pos/app/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.coyni.pos.app
+package com.coyni.pos.app.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.coyni.pos.app.R
 import com.coyni.pos.app.utils.MyApplication
 
 class MainActivity : AppCompatActivity() {
