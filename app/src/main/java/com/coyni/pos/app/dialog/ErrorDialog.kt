d4725dd1b976f6c82b15ba2ac6683cf5100a607c
Sriniv_A <srinivasa@ideyalabs.com>
2023-02-15 19:07:16 +0530

    OnBoard and Login Screens

---
+++ app/src/main/java/com/coyni/pos/app/dialog/ErrorDialog.kt
@@ -0,0 +1,16 @@
+package com.coyni.pos.app.dialog
+
+import android.content.Context
+import android.os.Bundle
+import com.coyni.pos.app.R
+import com.coyni.pos.app.baseclass.BaseDialog
+import com.coyni.pos.app.databinding.DialogErrorBinding
+
+class ErrorDialog(context: Context) : BaseDialog(context) {
+
+    override fun getLayoutId() = R.layout.dialog_error
+
+    override fun initViews() {
+//        TODO("Not yet implemented")
+    }
+}
