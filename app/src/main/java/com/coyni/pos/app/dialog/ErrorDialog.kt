d2a71f10707c77371ae9b575d6514e91f8594850
Sriniv_A <srinivasa@ideyalabs.com>
2023-02-16 10:16:11 +0530

    Onboard Screen and Login Screen validations

---
+++ app/src/main/java/com/coyni/pos/app/dialog/ErrorDialog.kt
@@ -0,0 +1,14 @@
+package com.coyni.pos.app.dialog
+
+import android.content.Context
+import com.coyni.pos.app.R
+import com.coyni.pos.app.baseclass.BaseDialog
+
+class ErrorDialog(context: Context) : BaseDialog(context) {
+
+    override fun getLayoutId() = R.layout.dialog_error
+
+    override fun initViews() {
+//        TODO("Not yet implemented")
+    }
+}
