ccce4072862f192833bfea343341347c44c25e21
Sriniv_A <srinivasa@ideyalabs.com>
2023-03-06 15:31:42 +0530

    dashboard changes

--- app/src/main/java/com/coyni/pos/app/model/login/LoginData.kt
+++ app/src/main/java/com/coyni/pos/app/model/login/LoginData.kt
@@ -2,14 +2,14 @@ package com.coyni.pos.app.model.login
 
 data class LoginData(
     var terminalKey: Int? = null,
-    val terminalName: String? = null,
+    var terminalName: String? = null,
     var terminalId: String? = null,
-    val merchantId: Int? = null,
-    val jwtToken: String? = null,
-    val status: String? = null,
-    val passwordFailedAttempts: Int? = null,
-    val message: Any? = null,
-    val dbaName: String? = null,
-    val companyName: String? = null,
-    val image: Any? = null
+    var merchantId: Int? = null,
+    var jwtToken: String? = null,
+    var status: String? = null,
+    var passwordFailedAttempts: Int? = null,
+    var message: Any? = null,
+    var dbaName: String? = null,
+    var companyName: String? = null,
+    var image: Any? = null
 )
