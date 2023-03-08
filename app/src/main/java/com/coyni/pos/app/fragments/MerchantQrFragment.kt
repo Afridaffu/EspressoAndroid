476e26cf54c05f5791b9aa1934e7d93b3150e640
VinayT <vinayt@ideyalabs.com>
2023-03-08 19:30:55 +0530

    QR Code changes

--- app/src/main/java/com/coyni/pos/app/fragments/MerchantQrFragment.kt
+++ app/src/main/java/com/coyni/pos/app/fragments/MerchantQrFragment.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -21,6 +23,11 @@ import com.coyni.pos.app.utils.MyApplication
 import com.coyni.pos.app.utils.Utils
 import com.coyni.pos.app.view.DashboardActivity
 import com.coyni.pos.app.viewmodel.GenerateQrViewModel
+import okhttp3.*
+import okio.ByteString
+import org.json.JSONException
+import org.json.JSONObject
+import java.util.concurrent.TimeUnit
 
 class MerchantQrFragment : BaseFragment() {
     lateinit var binding: MerchantQrBinding
@@ -58,29 +65,22 @@ class MerchantQrFragment : BaseFragment() {
         binding.idIVQrcode.setImageBitmap(Utils.convertBase64ToBitmap(myApplication!!.mCurrentUserData.generateQrResponseData?.image.toString()))
         webSocketUrl =
             myApplication!!.mCurrentUserData.generateQrResponseData?.mposWebsocket.toString()
+        Utils.qrUniqueCode = myApplication!!.mCurrentUserData.generateQrResponseData?.uniqueId
 //        binding.lottieAnimV.loop(false)
 
         startWebSocket(webSocketUrl)
+//        startWebSocket("ws://DEV23-API-GATEWAY-511281183.us-east-1.elb.amazonaws.com:58080/pos")
 
         Handler().postDelayed({
-            rotate = AnimationUtils.loadAnimation(context, R.anim.rotate)
-            binding.lottieAnimV.startAnimation(rotate)
+//            rotate = AnimationUtils.loadAnimation(context, R.anim.rotate)
+//            binding.lottieAnimV.startAnimation(rotate)
 
             binding.amountTV.text = amount
             binding.qrLL.visibility = View.VISIBLE
             binding.animationRL.visibility = View.GONE
             binding.discardSaleLL.setBackgroundResource(R.drawable.bg_greencolor_filled)
             binding.discardSaleLL.isEnabled = true
-
-            Handler().postDelayed({
-//                rotate = AnimationUtils.loadAnimation(context, R.anim.rotate)
-//                binding.lottieAnimV.startAnimation(rotate)
-//                binding.qrLL.visibility = View.GONE
-//                binding.animationRL.visibility = View.VISIBLE
-//                binding.waitingText.visibility = View.VISIBLE
-
-            }, 3000)
-        }, 3000)
+        }, 1000)
 
 
         binding.discardSaleLL.setOnClickListener {
@@ -138,4 +138,100 @@ class MerchantQrFragment : BaseFragment() {
             amount = java.lang.String.valueOf(requireArguments()[Utils.VALUE])
         }
     }
+
+    private class EchoWebSocketListener : WebSocketListener() {
+        private val interval = 2000
+        private val handler = Handler(Looper.getMainLooper())
+        private var runnable: Runnable? = null
+        private var CLOSE_STATUS = 1000
+        override fun onOpen(webSocket: WebSocket, response: Response) {
+            Log.e("onOpen", "onOpen")
+            try {
+                Log.e("authorization: ", Utils.strAuth.toString())
+                val jsonObject = JSONObject()
+                jsonObject.put("authorization", Utils.strAuth.toString())
+//                jsonObject.put("code", Utils.qrUniqueCode)
+                jsonObject.put("checkoutCode", Utils.qrUniqueCode)
+                Log.e("jsonObject", jsonObject.toString())
+                webSocket.send(jsonObject.toString())
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+        }
+
+        override fun onMessage(webSocket: WebSocket, message: String) {
+            Log.d("Receive Message: ", message)
+            try {
+                val obj = JSONObject(message)
+                if (obj.getString("eventType") == "SERVER_CONNECTION") {
+                    runnable = Runnable {
+                        try {
+                            val jsonObject = JSONObject()
+                            jsonObject.put("authorization", Utils.strAuth.toString())
+                            jsonObject.put("eventType", "ping")
+                            jsonObject.put("checkoutCode", Utils.qrUniqueCode)
+
+                            webSocket.send(jsonObject.toString())
+                            handler.postDelayed(runnable!!, interval.toLong())
+                        } catch (ex: java.lang.Exception) {
+                            ex.printStackTrace()
+                        }
+                    }
+                    handler.postDelayed(runnable!!, 0)
+                } else if (obj.getString("eventType") == "POS_TXN_STATUS"
+                ) {
+                    if (obj.getString("txnStatus").equals("Failed", true)) {
+
+                    } else if (obj.getString("txnStatus").equals("Success", true)) {
+
+                    }
+                    webSocket.close(CLOSE_STATUS, null)
+                } else if (obj.getString("eventType") == "SESSION_EXPIRY") {
+                    handler.removeCallbacks(runnable!!)
+                    webSocket.close(CLOSE_STATUS, null)
+                }
+            } catch (ex: java.lang.Exception) {
+                ex.printStackTrace()
+            }
+        }
+
+        override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
+            Log.d("Receive Bytes : ", bytes.hex())
+        }
+
+        override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
+            webSocket.close(CLOSE_STATUS, null)
+            handler.removeCallbacks(runnable!!)
+            Log.d("Closing Socket : ", "$code / $reason")
+        }
+
+        override fun onFailure(webSocket: WebSocket, throwable: Throwable, response: Response?) {
+            try {
+                //webSocket.close(CLOSE_STATUS, null);
+                if (throwable.message != null) Log.d(
+                    "Error : ",
+                    throwable.message!!
+                ) else Log.d("Error : ", throwable.toString())
+                handler.removeCallbacks(runnable!!)
+            } catch (ex: java.lang.Exception) {
+                ex.printStackTrace()
+            }
+        }
+    }
+
+    fun startWebSocket(url: String) {
+        try {
+            val serverUrl: String = url
+            val client: OkHttpClient =
+                OkHttpClient.Builder().readTimeout(0, TimeUnit.MILLISECONDS).build()
+            val request: Request = Request.Builder().url(serverUrl).build()
+            val listener = EchoWebSocketListener()
+            val webSocket = client.newWebSocket(request, listener)
+            client.dispatcher.executorService.shutdown()
+            Log.d("WebSocket Started with url : ", serverUrl)
+        } catch (ex: java.lang.Exception) {
+            ex.printStackTrace()
+        }
+    }
+
 }
