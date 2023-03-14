339e666a4182f2082ac55ca557b4bf5cdfc877da
Ganesh V <ganeshv@ideyalabs.com>
2023-03-14 13:12:54 +0530

    refund reference id change

--- app/src/main/java/com/coyni/pos/app/view/RefundTransactionActivity.kt
+++ app/src/main/java/com/coyni/pos/app/view/RefundTransactionActivity.kt
@@ -192,16 +192,25 @@ class RefundTransactionActivity : BaseActivity(), TextWatcher {
                 if (refundResponse != null) {
                     dismissDialog()
                     if (refundResponse.status == Utils.SUCCESS) {
-                        myApplication?.mCurrentUserData?.refundResponseData = refundResponse.data
-                        val intent = Intent(this, TransactionStatusActivity::class.java)
-                        intent.putExtra(Utils.SCREEN, Utils.REFUND)
-                        intent.putExtra(Utils.STATUS, Utils.SUCCESS)
-                        intent.putExtra(
-                            Utils.REFUNDED_AMOUNT,
-                            Utils.convertTwoDecimal(binding.refundAmountET.text.toString())
-                                .replace("$", "")
-                        )
-                        startActivity(intent)
+                        if (refundResponse.data?.referenceId != null || refundResponse.data?.referenceId != "") {
+                            myApplication?.mCurrentUserData?.refundResponseData =
+                                refundResponse.data
+                            val intent = Intent(this, TransactionStatusActivity::class.java)
+                            intent.putExtra(Utils.SCREEN, Utils.REFUND)
+                            intent.putExtra(Utils.STATUS, Utils.SUCCESS)
+                            intent.putExtra(
+                                Utils.REFUNDED_AMOUNT,
+                                Utils.convertTwoDecimal(binding.refundAmountET.text.toString())
+                                    .replace("$", "")
+                            )
+                            startActivity(intent)
+                        } else {
+                            Utils.displayAlert(
+                                refundResponse.error?.errorDescription.toString(),
+                                this,
+                                ""
+                            )
+                        }
                     } else {
                         Utils.displayAlert(
                             refundResponse.error?.errorDescription.toString(),
